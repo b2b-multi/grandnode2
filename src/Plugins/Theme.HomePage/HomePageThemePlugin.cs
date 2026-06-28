d141019a4e96d1275dadf0a170aec18a570ce7de
Nayeem <gdnayeem@gmail.com>
2026-06-28 10:37:09 +0600

    feat: HomePageTheme pulugin setup

---
+++ src/Plugins/Theme.HomePage/HomePageThemePlugin.cs
@@ -0,0 +1,8 @@
+using Grand.Infrastructure.Plugins;
+
+namespace Theme.HomePage;
+
+/// <summary>
+///     Plugin
+/// </summary>
+public class HomePageThemePlugin : BasePlugin, IPlugin;
---
+++ src/Plugins/Theme.HomePage/HomePageThemeView.cs
@@ -0,0 +1,22 @@
+﻿using Grand.Web.Common.Themes;
+
+namespace Theme.HomePage;
+
+public class HomePageThemeView : IThemeView
+{
+    public string AreaName => "";
+    public string ThemeName => "HomePage";
+
+    public ThemeInfo ThemeInfo => new("Home Page theme (beta)", "~/Plugins/Theme.HomePage/Content/theme.jpg",
+        "Home Page theme (beta)", false);
+
+    public IEnumerable<string> GetViewLocations()
+    {
+        return new List<string> {
+            "/Views/THomePage/{1}/{0}.cshtml",
+            "/Views/THomePage/Shared/{0}.cshtml",
+            "/Views/{1}/{0}.cshtml",
+            "/Views/Shared/{0}.cshtml"
+        };
+    }
+}
